0d47c3169559f2a8c6dc69c2fb7384f55bde0076
Danilo Silva <danilosantos14a@gmail.com>
2023-08-03 11:47:54 -0300

    Estrutura Sequencial

---
+++ EstruturaSequencial/Question2.java
@@ -0,0 +1,14 @@
+package EstruturaSequencial;
+
+public class Question2 {
+    public static void main(String[] args) {
+        double b, B, h, area;
+        b = 10.0;
+        B = 9.0;
+        h = 6.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+    }
+}
